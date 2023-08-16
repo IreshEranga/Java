c26e4047309b9674a7642df32c7ba50b7fb87fa8
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-17 00:13:58 +0530

    Create Main.java

---
+++ Labtest/version G/Main.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		//create objects
+		Employee emp1 = new Employee();
+		Manager mn1 = new Manager();
+		
+		//call methods
+		mn1.Read();
+		mn1.Print();
+		
+	}
+}
