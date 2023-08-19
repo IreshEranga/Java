575d17b2ce98513066a257e52bd80702c2bdaf37
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-19 23:05:07 +0530

    Create Student.java

---
+++ Lab/03/EX02/Student.java
@@ -0,0 +1,20 @@
+
+
+public class Student {
+    String name;
+    int ditNo;
+    String address;
+
+    //constructor
+    public Student(String name, int nditNo, String address)
+    {
+        this.name = name;
+        ditNo = nditNo;
+        this.address = address;
+
+        //If we use same variable that we declared in class to the constructor then we show it with this keyword
+        //otherwise we dont need this keyword
+
+
+    }
+}
