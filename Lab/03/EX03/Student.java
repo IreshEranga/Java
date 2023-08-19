6b3a8a8a3b0204d89604cecfbc060d3651428b88
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-19 23:38:50 +0530

    Create Student.java

---
+++ Lab/03/EX03/Student.java
@@ -0,0 +1,54 @@
+
+
+public class Student {
+    String name;
+    int ditNo;
+    String address;
+
+
+    //constructor
+    public Student()
+    {
+
+    }
+    public Student(String name, int nditNo, String address)
+    {
+        this.name = name;
+        ditNo = nditNo;
+        this.address = address;
+
+        //If we use same variable that we declared in class to the constructor then we show it with this keyword
+        //otherwise we dont need this keyword
+
+
+
+    }
+
+
+    //mutator methods
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    public void setDitNo(int newDit)
+    {
+        ditNo = newDit;
+    }
+    public void setAddress(String adrs)
+    {
+        address = adrs;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getDitNo()
+    {
+        return ditNo;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
+}
