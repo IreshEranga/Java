2301ee53fe8e4018b8aa927eea3491e4016ce137
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-19 23:04:31 +0530

    Create Test.java

---
+++ Lab/03/EX02/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        Student std = new Student("Iresh", 123456,"asdfrewq");
+
+        System.out.println("Name : "+std.name+"\nDIT No : "+std.ditNo+"\nAddress : "+std.address);
+    }
+}
