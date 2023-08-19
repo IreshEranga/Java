9e05ac93967358b09fc21c567ab4ffdf3a544dbd
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-19 23:38:10 +0530

    Create Test.java

---
+++ Lab/03/EX03/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args)
+    {
+        Student std = new Student();
+
+        std.setName("iresh Eranga Magage");
+        std.setDitNo(11100);
+        std.setAddress("Kasduwert");
+
+        System.out.println("Name : "+ std.getName()+"\nDit No : "+std.getDitNo()+"\nAddress : "+ std.getAddress());
+    }
+}
