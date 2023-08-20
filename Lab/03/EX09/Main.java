607892df5a0a52b723c9f4bc576108c36bee0c02
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-21 00:24:39 +0530

    Create Main.java

---
+++ Lab/03/EX09/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Pet p = new Pet("Lissie","Smith", 0);
+        p.showDetails();
+        Cat c = new Cat("Kyan", "Silva", 0,7);
+        c.showDetails();
+    }
+
+
+}
