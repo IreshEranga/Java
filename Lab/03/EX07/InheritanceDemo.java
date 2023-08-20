4e3c861e983c5faaa34ad4489936e3717ea6ded7
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-20 18:53:20 +0530

    Create InheritanceDemo.java

---
+++ Lab/03/EX07/InheritanceDemo.java
@@ -0,0 +1,8 @@
+public class InheritanceDemo {
+    public static void main(String[] args)
+    {
+        PartTimeStudent std = new PartTimeStudent("Iresh",22,"Kurunegala",15);
+
+        std.showDetails();
+    }
+}
