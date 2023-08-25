086da31ee633c80e960f7070cf58920e78a7c4f3
Iresh Eranga <iresheranga2020al@gmail.com>
2023-08-25 22:05:03 +0530

    Create Motorcycle.java

---
+++ Practise Codes/_11_/Motorcycle.java
@@ -0,0 +1,19 @@
+package p4;
+
+public class Motorcycle extends Vehicle {
+
+	private int engineCapacity;
+
+	
+
+	public Motorcycle(String model, double rentalPricePerDay, int engineCapacity) {
+		super(model, rentalPricePerDay);
+		this.engineCapacity = engineCapacity;
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+}
