90a8b59bbe341455e8779d98e04e8b27b5fa328f
Iresh Eranga <iresheranga2020al@gmail.com>
2023-11-11 17:02:35 +0530

    Add files via upload

---
+++ Final/2018/Ques01/Car.java
@@ -0,0 +1,26 @@
+package q1;
+
+public class Car extends Item{
+
+	private String model;
+	private String type;
+	
+	
+	public Car(int itemNo, String description, double unitPrice, String model, String type) {
+		super(itemNo, description, unitPrice);
+		this.model = model;
+		this.type = type;
+	}
+
+
+	@Override
+	public void Display() {
+		
+		super.Display();
+		System.out.println("Model : "+model);
+		System.out.println("Type : "+type);
+	}
+	
+	
+	
+}
